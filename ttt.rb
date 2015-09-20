4b9c6b10700f1fec8aa79c73b18c64d12d6d433f
Matt <mkweick@gmail.com>
2015-09-20 13:09:54 -0400

    Refactor tic-tac-toe game code

--- ttt.rb
+++ ttt.rb
@@ -1,28 +1,25 @@
-# Used to determine winner and calculate educated computer moves
 WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9],
                  [3,5,7]]
 
-# Initialize game board values
 def new_game_board
     positions = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 
                   7 => 7, 8 => 8, 9 => 9 }
     positions
 end
 
-# Ask player which character to use
 def get_player_letter
   puts "Would you like to be X's or O's? (X/O)"
   letter = ""
   loop do
     letter = gets.chomp.upcase
-    break if ["X", "O"].include?(letter)
+    break if %w(X O).include? letter
     puts "You can only select 'X' or 'O' as valid game pieces"
   end
   letter
 end
 
-# Draw game board to screen with interop hash values (from new_game_board)
 def build_board(board)
+  system "clear"
   puts "     |     |     "
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}   "
   puts "     |     |     "
@@ -36,20 +33,17 @@ def build_board(board)
   puts "     |     |     "
 end
 
-# Get player move
 def player_move(board, letter)
   player_input = valid_tile?(board)
   board[player_input] = letter
 end
 
-# Check to see if player move is valid with helpful re-prompt messages and inputs
 def valid_tile?(board)
-  puts "Specify the number on the board to begin:"
+  puts "Your turn, select a tile:"
   player_input = gets.chomp.to_i
   loop do
-    tile_valid = tile_in_one_thru_nine?(player_input)
+    tile_valid = tile_one_thru_nine?(player_input)
     unplayed_tile = tile_already_played?(board, player_input)
-    # Only break if both methods above are true
     break if tile_valid && unplayed_tile
     puts "Tile #{player_input} is not between 1-9 or has already been played."
     puts "Please enter a valid tile number:"
@@ -58,110 +52,75 @@ def valid_tile?(board)
     player_input
 end
 
-# Check to verify player has selected a tile between 1-9
-def tile_in_one_thru_nine?(tile)
-  if (1..9).none? { |number| number == tile }
-    return false
-  else
-    return true
-  end
+def tile_one_thru_nine?(tile)
+  (1..9).any? { |number| number == tile }
 end
 
-# Check to verify selected tile has not been played already
 def tile_already_played?(board, tile)
-  if board[tile] == "X" || board[tile] == "O"
-    return false
-  else
-    return true
-  end
+  %w(X O).none? { |letter| letter == board[tile] }
 end
 
-# Calculate educated computer move
 def computer_move(board, player_letter, computer_letter)
-  # Check for 2 values of computer character in same row
+  # Check 2 in a row for computer character
   if WINNING_LINES.any? { |two| board.values_at(*two).count(computer_letter) == 2 }
     check_for_computer_two_in_row(board, player_letter, computer_letter)
-  # Check for 2 values of player character in same row
+  # Check 2 in a row for player character
   elsif WINNING_LINES.any? { |two| board.values_at(*two).count(player_letter) == 2 }
     check_for_player_two_in_row(board, player_letter, computer_letter)
-  # Computer picks random tile
   else
     assign_random_computer_tile(board, computer_letter)
   end
 end
 
-# check to see if computer can win, then move to win game
+# Check if computer can win
 def check_for_computer_two_in_row(board, player_letter, computer_letter)
-  # Select rows with 2 computer characters
   two_in_row = WINNING_LINES.select do |two| 
     board.values_at(*two).count(computer_letter) == 2
   end
-  # From selected rows above, filter only rows where third tile doesn't contain player letter
   two_in_row_open = two_in_row.select do |two| 
     two.none? do |number| 
-      board.values_at(number).include?(player_letter)
+      board.values_at(number).include? player_letter
     end
   end
-  # Rows selected are already blocked by player, run possible player win method
   if two_in_row_open.empty?
     check_for_player_two_in_row(board, player_letter, computer_letter)
-  # Block player row with 2 in a row
   else
-    # If multiple rows found, select the first row only
     two_in_row_final = two_in_row_open.first
-    # From final selected row, select only the tile # that contains an integer
-    comp_tile_number = two_in_row_final.select { |tile| (1..9).include?(board[tile]) }
-    # Pull-out the tile # from array format to integer
+    comp_tile_number = two_in_row_final.select { |tile| (1..9).include? board[tile] }
     computer_input = comp_tile_number.first
-    # Return board hash key and value
     board[computer_input] = computer_letter
   end
 end
 
-#check to see if player can win, then move to possibly prevent player win
+# Check if player can win
 def check_for_player_two_in_row(board, player_letter, computer_letter)
-  # Select rows with 2 player characters
   two_in_row = WINNING_LINES.select do |two| 
     board.values_at(*two).count(player_letter) == 2
   end
-  # From selected rows above, filter only rows where third tile doesn't contain computer letter
   two_in_row_open = two_in_row.select do |two| 
     two.none? do |number| 
-      board.values_at(number).include?(computer_letter)
+      board.values_at(number).include? computer_letter
     end
   end
-  # Rows selected are already blocked by computer, go for random tile
   if two_in_row_open.empty?
     assign_random_computer_tile(board, computer_letter)
-  # Block player row with 2 in a row
   else
-    # If multiple rows found, select the first row only
     two_in_row_final = two_in_row_open.first
-    # From final selected row, select only the tile # that contains an integer
-    comp_tile_number = two_in_row_final.select { |tile| (1..9).include?(board[tile]) }
-    # Pull-out the tile # from array format to integer
+    comp_tile_number = two_in_row_final.select { |tile| (1..9).include? board[tile] }
     computer_input = comp_tile_number.first
-    # Return board hash key and value
     board[computer_input] = computer_letter
   end
 end
 
-# Generate a random available tile for computer
 def assign_random_computer_tile(board, computer_letter)
-  computer_input = board.select { |k, v| (1..9).include?(v) }.keys.sample
+  computer_input = board.select { |k, v| (1..9).include? v }.keys.sample
   board[computer_input] = computer_letter
 end
 
-# Check to see if all tiles have been played
-def all_tiles_played?(board)
-  if board.values.any? { |number| (1..9).include?(number) }
-    return false
-  else
-    return true
-  end
+def open_tiles?(board)
+  board.values.any? { |number| (1..9).include? number }
 end
 
-# Check for winner, return correct winner name
 def check_winner(board, player_letter, computer_letter, user_name)
   WINNING_LINES.each do |line|
     return user_name if board.values_at(*line).count(player_letter) == 3
@@ -170,7 +129,6 @@ def check_winner(board, player_letter, computer_letter, user_name)
   nil
 end
 
-# Display winner name
 def announce_winner(winner)
   puts "#{winner} won!"
 end
@@ -182,7 +140,6 @@ user_name = gets.chomp.capitalize
 # Start Game Loop
 loop do
   player_letter = get_player_letter
-  # Assign computer opposite letter of player
   player_letter == "X" ? computer_letter = "O" : computer_letter = "X"
   puts "#{user_name}, you will be #{player_letter}'s."
   puts
@@ -195,36 +152,24 @@ loop do
     player_move(board, player_letter)
     build_board(board)
     winner = check_winner(board, player_letter, computer_letter, user_name)
-    puts
-    # Only allow computer to play if open tiles are left or player didn't win
-    if !all_tiles_played?(board) && winner == nil
+    if open_tiles?(board) && winner.nil?
       puts "Computer is thinking......"
-      puts
       sleep 3.0
       computer_move(board, player_letter, computer_letter)
       build_board(board)
       winner = check_winner(board, player_letter, computer_letter, user_name)
-      # Announce computer as the winner
-      if winner != nil
-        announce_winner(winner)
-      end
-      puts
-    # Announce user_name (player) as the winner
-    elsif winner != nil
+      announce_winner(winner) if winner
+    elsif winner
       announce_winner(winner)
-    # Declare a tie game, no winner and all tiles played
     else
       puts "It's a tie."
-      puts
     end
-  # Only end if game board is full or someone has won
-  end until all_tiles_played?(board) || winner != nil
+  end until !open_tiles?(board) || winner
 
-  # Check to see if user want to play again
+  # Play again?
   begin
     puts "Do you want to play again? (Y/N)"
     play_again = gets.chomp.upcase
-  end until ["Y", "N"].any? { |letter| letter == play_again }
+  end until %w(Y N).any? { |letter| letter == play_again }
   break if play_again == "N"
-
 end
